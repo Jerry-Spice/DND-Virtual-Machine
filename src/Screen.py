d90ed1370d7bb818dacc2ab457467001f959bf88
Jerry Spice <36059502+Jerry-Spice@users.noreply.github.com>
2025-06-02 15:02:32 -0400

    Added Vector2 and added some methods to Screen

--- src/Screen.py
+++ src/Screen.py
@@ -1,4 +1,13 @@
 import pygame
 class Screen(pygame.Surface):
-    def __init__(self, name, width, height):
+    def __init__(self, name, x,y, width, height):
         super().__init_((width,height))
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.name = name
+    
+    def display(self, root : pygame.Surface):
+        root.blit(self.x, self.y)
+    
